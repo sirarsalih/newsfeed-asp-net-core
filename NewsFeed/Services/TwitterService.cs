292dd3041c19b681c964bb2816ab20cec96d0150
sirar87 <sirars@gmail.com>
2017-08-18 14:08:51 +0200

    Initial commit.

---
+++ NewsFeed/Services/TwitterService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewsFeed.Services.Interfaces;
+
+namespace NewsFeed.Services
+{
+    public class TwitterService : ITwitterService
+    {
+    }
+}
